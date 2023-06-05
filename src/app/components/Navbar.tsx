fc1fc63a9f14d8937c46a9c7fe4ae7138f40bb88
seon-mikim <tjsalsun@gmail.com>
2023-06-06 05:28:46 +0900

    refator: 로그인 페이지와 로그인 페이지에서
    - 네브바 렌더링 안되게 조건문 생성

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import NavItem from './NavItem'
+import { usePathname } from 'next/navigation'  
 import {HiHome} from 'react-icons/hi'
 import {HiHeart} from 'react-icons/hi'
 import {HiSearch} from 'react-icons/hi'
@@ -8,13 +9,19 @@ import {HiChartBar} from 'react-icons/hi'
 import {HiUser} from 'react-icons/hi'
 
 export default function Navbar() {
+  const pathname = usePathname()
   return (
-    <div className='flex justify-between text-center fixed bottom-0 w-screen mb-4'>
-        <NavItem name='전체뉴스' param='/' Icon={HiHome}/>
-        <NavItem name='관심뉴스' param='/favoriteNews' Icon={HiHeart}/>
-        <NavItem name='검색' param='/search' Icon={HiSearch}/>
-        <NavItem name='차트' param='/chart' Icon={HiChartBar}/>
-        <NavItem name='My' param='/my' Icon={HiUser}/>
+    <div>
+      {
+        pathname !== '/signup' && pathname !== '/login'&&
+        <div className='flex justify-between text-center fixed bottom-0 w-screen mb-4'>
+          <NavItem name='전체뉴스' param='/' Icon={HiHome}/>
+          <NavItem name='관심뉴스' param='/favoriteNews' Icon={HiHeart}/>
+          <NavItem name='검색' param='/search' Icon={HiSearch}/>
+          <NavItem name='차트' param='/chart' Icon={HiChartBar}/>
+          <NavItem name='My' param='/my' Icon={HiUser}/>
+        </div>
+      }
     </div>
   )
 }
