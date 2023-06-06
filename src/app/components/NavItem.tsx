011066e257a3a00f9ab0aff5987081ee089b0138
seon-mikim <tjsalsun@gmail.com>
2023-06-07 03:49:57 +0900

    refactor: constants 데이터 내려줌으로 인해
    - 해당 구조 변경

--- src/app/components/NavItem.tsx
+++ src/app/components/NavItem.tsx
@@ -1,29 +1,28 @@
-'use client'
+'use client';
 
-import Link from "next/link"   
-import { usePathname } from 'next/navigation'                                                                                
-import { IconType } from 'react-icons/lib'
-
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+import { IconType } from 'react-icons/lib';
 
 interface NavItemProps {
-    name: string
-    param: string
-    Icon:IconType
+  navItem: {
+    pathName: string;
+    name: string;
+    Icon: IconType;
+  };
 }
 
-export default function NavItem({name, param, Icon}:NavItemProps) {
- const pathName = usePathname()
+export default function NavItem({ navItem }: NavItemProps) {
+  const pathName = usePathname();
 
   return (
-  <div className="mx-auto">
-      <Link href={param} className={`${pathName === param?  'text-black': 'text-neutral-400'}`} >
+    <div key={navItem.pathName} className="mx-auto">
+      <Link href={navItem.pathName} className={`${pathName === navItem.pathName ? 'text-black' : 'text-neutral-400'}`}>
         <div className="w-10 h-10">
-         <Icon className="text-xl text-center mx-auto"/>
-          <span className='text-[10px] text-center font-bold'>
-            {name}
-          </span>
+          <navItem.Icon className="text-xl text-center mx-auto" />
+          <span className="text-[10px] text-center font-bold">{navItem.name}</span>
         </div>
       </Link>
-  </div>
-  )
+    </div>
+  );
 }
